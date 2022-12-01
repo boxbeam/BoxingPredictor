e620ae463c29185826b23204c3c49a7c85afd7ee
Redempt <instantiable@gmail.com>
2022-12-01 17:08:54 -0500

    Include year

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ import numpy as np
 data = pd.read_csv("BoxingData.csv")
 data.info()
 
-data.drop("Debut", axis=1, inplace=True)
-
 x = np.array(data.drop("Win Rate", axis=1))
 y = np.array(data["Win Rate"])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y)
 
-model = DecisionTreeRegressor(max_depth=5)
+model = DecisionTreeRegressor(random_state=42)
 model.fit(x_train, y_train)
 print(model.score(x_test, y_test))
