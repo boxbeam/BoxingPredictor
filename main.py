78b800ae9e0902a291391c4151082a02affbff4f
Grant Mahoney <grant.mahoney@mymail.champlain.edu>
2022-12-05 19:29:21 -0500

    Tree plot

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from sklearn.tree import DecisionTreeRegressor
+from sklearn import tree
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import figure
 
 data = pd.read_csv("BoxingData.csv")
 data.info()
@@ -14,3 +17,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y)
 model = DecisionTreeRegressor(random_state=42)
 model.fit(x_train, y_train)
 print(model.score(x_test, y_test))
+
+figure = plt.figure(figsize=(50,45))
+tree.plot_tree(model, filled=True, fontsize=5)
+plt.show()
